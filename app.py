b3dd68d4a2dc8899336687c19677cfe1d3554b62
William Tom <wzt8618@rit.edu>
2021-04-13 21:39:33 -0400

    Fixed @app.route("/admin")

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 from flask import Flask, render_template, redirect, url_for
 app = Flask(__name__)
 
+# Home Page
 @app.route("/")
 def home():
     return render_template('index.html')
@@ -12,10 +13,10 @@ def home():
 def user (name):
     return f"Hello {name}!"
 
+
 @app.route("/admin")
 def admin():
-    return redirect(url_for("/"))
+    return redirect(url_for("home"))  # redirect to home page
 
 if __name__ == "__main__":
     app.run()
-
